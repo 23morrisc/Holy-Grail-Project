f3a8ff49da6bf778cddf6c186d19c299c7272147
23morrisc <57642421+23morrisc@users.noreply.github.com>
2021-09-15 10:46:33 -0400

    Add files via upload
    updated the strings that were printed.

--- HolyGrail.java
+++ HolyGrail.java
@@ -10,15 +10,16 @@ public class HolyGrail {
         Scanner input = new Scanner(System.in);
         System.out.println("* a chat with the bridge keeper *");
         System.out.println("Who would cross the Bridge of Death must answer me these questions three, ere the other side he see.");
-        System.out.println("What is your name?");
+        System.out.println("Question 1: What is your name?");
         String name = input.nextLine();
-        System.out.println("What is your quest?");
+        System.out.println("Question 2: What is your quest?");
         String quest = input.nextLine();
-        System.out.println("What is your favorite color?");
+        System.out.println("Question 3: What is your favorite color?");
         String color = input.nextLine();
         System.out.println("King Arthur says, \"You have to know these things when you're a king, you know.\"");
         System.out.println("Your name is: " + name);
         System.out.println("Your quest is: " + quest);
         System.out.println("Your favorite color is: " + color);
+        System.out.println("* end of program *");
     }
 }
